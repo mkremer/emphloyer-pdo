6015d5aaf27785e909ce14e869fa61b3f6a68de1
Mark Cornelissen <cornelissen@srs.nl>
2018-12-19 12:32:18 +0100

    Changes to work with the latest changes in Emphloyer

--- tests/Emphloyer/Pdo/SchedulerBackendTest.php
+++ tests/Emphloyer/Pdo/SchedulerBackendTest.php
@@ -9,7 +9,8 @@ class SchedulerBackendTest extends \Emphloyer\Scheduler\BackendTestCase
         $this->pdo = new \PDO($GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD']);
         $this->pdo->exec('DROP TABLE IF EXISTS test_scheduled_jobs');
         $this->pdo->exec('CREATE TABLE test_scheduled_jobs (id INT AUTO_INCREMENT, uuid VARCHAR(36) UNIQUE, created_at TIMESTAMP, locked_at TIMESTAMP NULL DEFAULT NULL, lock_uuid VARCHAR(36), class_name VARCHAR(255), attributes TEXT, minute TINYINT(1) DEFAULT NULL, hour TINYINT(1) DEFAULT NULL, monthday TINYINT(1) DEFAULT NULL, month TINYINT(1) DEFAULT NULL, weekday TINYINT(1) DEFAULT NULL, PRIMARY KEY (id));');
-        $this->backend = new SchedulerBackend($GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD']);
+        $this->backend = new SchedulerBackend($GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'], [],
+            'test_scheduled_jobs');
         parent::setUp();
     }
 
